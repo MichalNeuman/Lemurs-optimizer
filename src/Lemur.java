2588f0a5404604bd2e254073bb7cafd7ea63b5da
Michał Neuman <michal.neuman@edu.uekat.pl>
2024-09-25 13:11:20 +0200

    Dodanie prostej funkcji fitnes

--- src/Lemur.java
+++ src/Lemur.java
@@ -2,32 +2,42 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Lemur {
-     Double[] valueOfDimensions;
+
+    Double fitness;
+     double[] valueOfDimensions;
 
 
     Random rnd=new Random();
     public Lemur(LemurConfig config){
-        valueOfDimensions=new Double[config.getNumberOfDimensions()];
+        valueOfDimensions=new double[config.getNumberOfDimensions()];
         for(int i =0;i<valueOfDimensions.length;i++){
             valueOfDimensions[i]= rnd.nextDouble() *
                     (config.getUpperBound() - config.getLowerBound())
                     + config.getLowerBound();
-
         }
-
-
     }
     public static void initializePopulation(LemurConfig config){
         ArrayList<Lemur> list = new ArrayList<>();
         for(int i =0;i<config.getNumberOfSolutions();i++){
            list.add(new Lemur(config));
+
         }
         for (Lemur x:list){
             for (int i=0;i<x.valueOfDimensions.length;i++){
                 System.out.print(x.valueOfDimensions[i]+" ");
+
             }
-            System.out.println();
+            System.out.println(x.calculateFitessValue());
+
         }
     }
+    public double calculateFitessValue(){
+        double value=0;
+        for(Double x: this.valueOfDimensions){
+            value+=x;
+        }
+        return value;
+    }
+
 
 }
